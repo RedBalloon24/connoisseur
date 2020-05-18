4002a17bd0d3805a185b4d74cce68dda9059ec3e
Anonymous <worldtraveler757@gmail.com>
2020-05-18 15:15:27 +0200

    add update image feature

--- controllers/posts.js
+++ controllers/posts.js
@@ -40,15 +40,41 @@ module.exports = {
         let post = await Post.findById(req.params.id);
         res.render('posts/edit', { post });
     },
-     // POSTS /update
-     async postUpdate(req, res, next) {
-        //handle any deletion of existing images
-        //handle upload of any new images
-        let post = await Post.findByIdAndUpdate(req.params.id, req.body.post);
+    // POSTS /update
+    async postUpdate(req, res, next) {
+        let post = await Post.findById(req.params.id);
+        if(req.body.deleteImages && req.body.deleteImages.length) {
+            let deleteImages = req.body.deleteImages;
+            for(const public_id of deleteImages) {
+                await cloudinary.v2.uploader.destroy(public_id);
+                for(const image of post.images) {
+                    if(image.public_id === public_id) {
+                        let index = post.images.indexOf(image);
+                        post.images.splice(index, 1);
+                    }
+                }
+            }
+        }
+
+        if(req.files) {
+            for(const file of req.files){
+                let image = await cloudinary.v2.uploader.upload(file.path);
+                post.images.push({
+                    url: image.secure_url,
+                    public_id: image.public_id
+                });
+            }
+        }
+        post.title = req.body.post.title;
+        post.description = req.body.post.description;
+        post.price = req.body.post.price;
+        post.location = req.body.post.location;
+        post.save();
+
         res.redirect(`/posts/${post.id}`);
     },
-     // POSTS /delete
-     async postDestroy(req, res, next) {
+    // POSTS /delete
+    async postDestroy(req, res, next) {
         let post = await Post.findByIdAndRemove(req.params.id);
         res.redirect('/posts');
     }
