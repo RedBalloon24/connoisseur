8af1206912e029479e32eb8297edd66f44c421e9
Anonymous <worldtraveler757@gmail.com>
2020-05-20 13:37:33 +0200

    add post coordinates to map

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,11 +1,12 @@
 const Post = require('../models/post');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const geocodingClient = mbxGeocoding({ accessToken: process.env.MAPBOX_TOKEN });
 const cloudinary = require('cloudinary');
-
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
+    cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    })
 
 module.exports = {
     // POSTS /index
@@ -27,6 +28,13 @@ module.exports = {
                 public_id: image.public_id
             });
         }
+        let response = await geocodingClient
+            .forwardGeocode({
+                query: req.body.post.location,
+                limit: 1
+            })
+            .send();
+        req.body.post.coordinates = response.body.features[0].geometry.coordinates;
         let post = await Post.create(req.body.post);
         res.redirect(`/posts/${post.id}`);
     },
