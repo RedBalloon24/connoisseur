f203f134790c0a2e5a259d5225465a26141864b8
Anonymous <worldtraveler757@gmail.com>
2020-05-23 23:44:11 +0200

    add clear rating button

--- public/javascripts/post-show.js
+++ public/javascripts/post-show.js
@@ -23,3 +23,8 @@ $('.toggle-edit-form').on('click', function() {
     $(this).text() === 'Edit' ? $(this).text('Cancel') : $(this).text('Edit');
     $(this).siblings('.edit-review-form').toggle();
 })
+
+// add click listener for clearing of rating from edit/new form
+$('.clear-rating').click(function() {
+    $(this).siblings('.input-no-rate').click();
+});
